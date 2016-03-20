c9f58c00ac591cc6e7007373f81d744605b86d46
omar <og39@njit.edu>
2016-03-19 22:27:13 -0400

    Updated add/delete routine, add sets, progress reports

--- testini.php
+++ testini.php
@@ -4,5 +4,7 @@ require_once("newUser.php.inc");
 
 $workoutDB = new workoutDB("workoutDB.ini");
 
-$workoutDB->addUser("og39","Omar","Gomez",1234567890,"trainer","boom");
+//$workoutDB->createRoutine("og39","boom","routine1");
+
+$workoutDB->addSet('og39','boom','routine1','2016-03-04','dbbench',1,100,5,'chest');
 ?>
